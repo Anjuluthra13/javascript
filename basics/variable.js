049b03ff75d3566b5fa7dcb0c9b6a6967cc7e568
Anjuluthra13 <122430165+Anjuluthra13@users.noreply.github.com>
2024-07-02 04:15:23 +0000

    variable

---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountID = 124578
+let accountName = "MERRA"
+var accountEmail ="Merra@gmail.com"
+accountCity = "Delhi"
+let age;
+console.log(accountID)
+accountEmail = "Anju@gmail.com"
+accountName = "Anju"
+accountCity = "New York"
+
+//accountID = 1234567
+console.table([accountEmail,accountID,accountName,accountCity,age]);
+
